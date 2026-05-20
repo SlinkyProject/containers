a39c6a9e04713147950a762b1ad66a22d544107a
Alan Mutschelknaus <amutschelkna@nvidia.com>
2026-05-20 08:25:06 -0500

    feat: add SSSD_MODE to enable embedded, sidecar, or disabled modes

--- schedmd/slurm/master/ubuntu26.04/files/usr/local/bin/login-entrypoint.sh
+++ schedmd/slurm/master/ubuntu26.04/files/usr/local/bin/login-entrypoint.sh
@@ -9,6 +9,25 @@ export SSHD_OPTIONS="${SSHD_OPTIONS:-""}"
 export SACKD_OPTIONS="${SACKD_OPTIONS:-""}"
 export SSSD_OPTIONS="${SSSD_OPTIONS:-""}"
 
+function configure_sssd() {
+	local sssd_mode="${SSSD_MODE:-embedded}"
+	local available_conf="/etc/supervisor/conf.available/sssd.conf"
+	local enabled_conf="/etc/supervisor/conf.d/sssd.conf"
+
+	rm -f "$enabled_conf"
+
+	case "$sssd_mode" in
+	embedded)
+		ln -s "$available_conf" "$enabled_conf"
+		;;
+	sidecar | disabled) ;;
+	*)
+		echo "unsupported SSSD_MODE: ${sssd_mode}" >&2
+		exit 1
+		;;
+	esac
+}
+
 function main() {
 	mkdir -p /run/sshd/
 	chmod 0755 /run/sshd/
@@ -16,6 +35,7 @@ function main() {
 	rm -f /var/run/dbus/pid
 
 	ssh-keygen -A
+	configure_sssd
 
 	exec supervisord -c /etc/supervisor/supervisord.conf
 }
